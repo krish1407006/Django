d042ad06530294acdbb9bd4265d44ddb94da81df
krish1407006 <krish14072006@gmail.com>
2026-07-08 23:04:22 +0530

    gh

--- myproject/store/models.py
+++ myproject/store/models.py
@@ -2,6 +2,7 @@ from django.db import models
 class category(models.Model):
     name=models.CharField(max_length=100)
     description = models.TextField()
+    price = models.IntegerField()
 
     def __str__(self):
         return self.name
