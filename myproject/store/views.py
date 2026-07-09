883f6869e58f1114846f667ef186a539934eec6d
krish1407006 <krish14072006@gmail.com>
2026-07-09 21:34:05 +0530

    dj

--- myproject/store/views.py
+++ myproject/store/views.py
@@ -1,24 +1,35 @@
 from django.shortcuts import render
 
+from .models import Product
+
 def home(request):
     return render(request, "store/home.html")
 
+
 def home(request):
+    products = Product.objects.all()
+
     context = {
-        "name": "krish",
-        "course": "Django",
-        "date": "2024-06-10",
-        "city": "hoshiarpur",
-        "college":"RBPU",
-        "is_student":False,
-        "age": 20,
-        "marks": 90
-
-
-
+        "products": products
     }
+
     return render(request, "store/home.html", context)
 
+# def home(request):
+#     context = {
+#         "name": "krish",
+#         "course": "Django",
+#         "date": "2024-06-10",
+#         "city": "hoshiarpur",
+#         "college":"RBPU",
+#         "is_student":False,
+#         "age": 20,
+#         "marks": 90
+
+
+
+#     }
+#     return render(request, "store/home.html", context)
 
 
 
