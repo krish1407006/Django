1547f7d08629167076b630322758f5ef2b69d7c6
krish1407006 <krish14072006@gmail.com>
2026-07-16 00:57:05 +0530

    dj

--- myproject/blogproject/blog/views.py
+++ myproject/blogproject/blog/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse("Hello, World! This is the home page of the blog.")
+    return HttpResponse("Hello, World! This is the home page of  blog.")
 # Create your views here.
