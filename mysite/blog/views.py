97a72bfa1ccdea898bd3e0e82fb72aa3a8436146
krish1407006 <krish14072006@gmail.com>
2026-07-23 22:25:33 +0530

    dj

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -7,6 +7,16 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, "blog/home.html")
+    context = {
+        "name": "krish",
+        "course": "Django",
+        # "date": "2024-06-10",
+        # "city": "hoshiarpur",
+        # "college":"RBPU",
+        # "is_student":False,
+        # "age": 20,
+        # "marks": 90
+    }
+    return render(request, "blog/home.html", context)
 
 # Create your views here.
