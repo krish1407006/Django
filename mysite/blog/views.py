8537a70a29081a5a8c8141f7be13c6a938f975ff
krish1407006 <krish14072006@gmail.com>
2026-07-23 22:22:57 +0530

    dj

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,7 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 
-def home(request):
-    return HttpResponse("Hello, World! This is the home page of the blog app.")
+# def home(request):
+#     return HttpResponse("Hello, World! This is the home page of the blog app.")
 
 # Create your views here.
