bbff3043f11c23dea198dd3a8e1df292d7f77e71
krish1407006 <krish14072006@gmail.com>
2026-07-23 22:24:48 +0530

    dj

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,4 +4,9 @@
 # def home(request):
 #     return HttpResponse("Hello, World! This is the home page of the blog app.")
 
+from django.shortcuts import render
+
+def home(request):
+    return render(request, "blog/home.html")
+
 # Create your views here.
