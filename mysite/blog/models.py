1b81e979be102a6229d660e203a24409ddba3b03
krish1407006 <krish14072006@gmail.com>
2026-08-04 21:45:57 +0530

    dj

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+    class Post(models.Model):
+        title = models.CharField(max_length=100)
+        content = models.TextField()
+        date_posted = models.DateTimeField(auto_now_add=True)
+        author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+        def __str__(self):
+            return self.title
 # Create your models here.
